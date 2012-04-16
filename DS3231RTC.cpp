1c155bb39793c5b77c8df11f36a42837052f2b54
Wagner Sartori Junior <wsartori@gmail.com>
2012-04-15 21:52:35 -0300

    Support arduino 1.0

--- DS3231RTC.cpp
+++ DS3231RTC.cpp
@@ -19,7 +19,11 @@
 
   You should have received a copy of the GNU General Public License
 */
+#if ARDUINO >= 100
+#include <Arduino.h>
+#else
 #include <WProgram.h>
+#endif
 
 #include <Wire.h>
 #include "DS3231RTC.h"
